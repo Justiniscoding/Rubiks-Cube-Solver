ba036d74cb934b181836f7538e68633f5adabb9d
Justiniscoding <justinlasinski@gmail.com>
2026-03-03 11:23:31 -0500

    fix: add clang-format for arduino formatting with tabs

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,11 +4,11 @@ Servo bottom;
 Servo top;
 
 void setup() {
-  pinMode(52, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(52, INPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  bottom.attach(8);
-  top.attach(10);
+	bottom.attach(8);
+	top.attach(10);
 }
 
 bool canRun = true;
@@ -17,18 +17,18 @@ int num = 0;
 int lastState;
 
 void loop() {
-  int buttonState = digitalRead(52);
+	int buttonState = digitalRead(52);
 
-  if (lastState == LOW && buttonState == HIGH) {
-    top.write(180);
-    delay(500);
-    bottom.write(60);
-    delay(500);
-    top.write(90);
-  } else {
-    bottom.write(90);
-    top.write(90);
-  }
+	if (lastState == LOW && buttonState == HIGH) {
+		top.write(180);
+		delay(500);
+		bottom.write(60);
+		delay(500);
+		top.write(90);
+	} else {
+		bottom.write(90);
+		top.write(90);
+	}
 
-  lastState = buttonState;
+	lastState = buttonState;
 }
